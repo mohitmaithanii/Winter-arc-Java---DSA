32255d26713d9e8848d264d04804babd043d111d
mohitmaithanii <mohitmaithani5@gmail.com>
2024-10-01 12:00:18 +0530

    hello world in java

---
+++ Java basics/HelloWorld.java
@@ -0,0 +1,9 @@
+// This is the definition of a new Java class called HelloWorld
+
+public class HelloWorld {
+    // This is the main method, which is the entry point of the program
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
