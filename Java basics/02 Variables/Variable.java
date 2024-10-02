103d3db06fd04a57a0139daac4f713a61f12bd0f
mohitmaithanii <mohitmaithani5@gmail.com>
2024-10-02 10:19:56 +0530

    java datatypes

--- Java basics/02 Variables/Variable.java
+++ Java basics/02 Variables/Variable.java
@@ -1,23 +1,33 @@
 // Java Variables :
 // A variable is a name given to a memory location that stores a value. Variables are used to store and manipulate data in a Java program. A variable has a name, a datatype, and a value.
 
-public class variables {
+public class Variable {
    public static void main(String[] args) {
 
       // Variable names can start with letters (both lowercase and uppercase)
       int age = 22;
       int AGE = 20;
 
+      System.out.println("age : " + age);
+      System.out.println("AGE : " + AGE);
+
       // Variable names can also start with an underscore (_)
       int _marks = 80;
+      System.out.println("_marks : " + _marks);
 
       // Variable names can use camelCase notation
       int firstMark = 83;
       int secondMark = 58;
 
+      System.out.println("firstMark : " + firstMark);
+      System.out.println("secondMark : " + secondMark);
+
       // Variable names can start with a dollar sign ($)
       int $score = 100;
       int score$allowed = 23;
+
+      System.out.println("$score : " + $score);
+      System.out.println("score$allowed : " + score$allowed);
 
    }
 }
---
+++ Java basics/03 Data Types/DataTypes.java
@@ -0,0 +1,71 @@
+// Java Data Types :
+// a data type is a category of data that defines how the data should be stored and what type of value it can hold. 
+// There are two main categories of data types in Java: Primitive Data Types and Non-Primitive Data Types.
+
+public class DataTypes {
+
+   public static void main(String[] args) {
+
+      // 1. int - whole numbers, e.g. 1, 2, 3, etc.
+      int count = 12;
+      System.out.println("int: " + count);
+
+      // 2. float - decimal numbers, e.g. 3.14, -0.5, etc.
+      // Note: 'f' is used to specify a float value
+      float percentage = 16.5f;
+      System.out.println("float: " + percentage);
+
+      // 3. char - single characters, e.g. 'a', 'A', '0', etc.
+      // Note: char values are enclosed in single quotes
+      char grade = 'A';
+      System.out.println("char: " + grade);
+
+      // 4. boolean - true or false values
+      boolean isPassed = true;
+      System.out.println("Boolean: " + isPassed);
+
+      // 5. byte - whole numbers, e.g. 1, 2, 3, etc. (range: -128 to 127)
+      byte marks = 13;
+      System.out.println("byte: " + marks);
+
+      // 6. short - whole numbers, e.g. 1, 2, 3, etc. (range: -32768 to 32767)
+      short score = 14;
+      System.out.println("short: " + score);
+
+      // 7. long - whole numbers, e.g. 1, 2, 3, etc. (range: -9223372036854775808 to 9223372036854775807)
+      // Note: 'L' is used to specify a long value
+      long id = 15L;
+      System.out.println("long: " + id);
+
+      // 8. double - decimal numbers, e.g. 3.14, -0.5, etc.
+      double pi = 3.14159265358979323846;
+      System.out.println("double: " + pi);
+
+   }
+}
+
+// Types of Data Types:
+
+// 1. Primitive Data Types
+/*
+ * 1. int - whole numbers, e.g. 1, 2, 3, etc.
+ * 2. float - decimal numbers, e.g. 3.14, -0.5, etc.
+ * 3. char - single characters, e.g. 'a', 'A', '0', etc.
+ * 4. boolean - true or false values
+ * 5. byte - whole numbers, e.g. 1, 2, 3, etc. (range: -128 to 127)
+ * 6. short - whole numbers, e.g. 1, 2, 3, etc. (range: -32768 to 32767)
+ * 7. long - whole numbers, e.g. 1, 2, 3, etc. (range: -9223372036854775808 to
+ * 9223372036854775807)
+ * 8. double - decimal numbers, e.g. 3.14, -0.5, etc.
+ * 
+ */
+
+// 2. Non-Primitive Data Types
+/*
+ * 1. String - a sequence of characters, e.g. "hello", 'hello', etc.
+ * 2. Array - a collection of values of the same type, e.g. int[], double[],etc.
+ * 3. List - a collection of values that can be of different types, e.g.
+ * ArrayList, LinkedList, etc.
+ * 4. Set - a collection of unique values, e.g. HashSet, TreeSet, etc.
+ * 5. Map - a collection of key-value pairs, e.g. HashMap, TreeMap, etc.
+ */
