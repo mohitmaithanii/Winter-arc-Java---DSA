d4e663cb9bf61f1d9579faa81f8c3321cf60ac2e
mohitmaithanii <mohitmaithani5@gmail.com>
2024-10-11 16:32:14 +0530

    Comparing Strings in Java

---
+++ 08 String/02 Comparing Two Strings/ComparingString.java
@@ -0,0 +1,31 @@
+// Comparing Strings in Java :
+
+public class ComparingString {
+
+   public static void main(String[] args) {
+
+      // Create two strings with the same value, but one is a literal and the other is created using the 'new' keyword
+      String name = "Mohit"; 
+      String sameName = "Mohit"; 
+      String newName = new String("Mohit"); 
+
+      // Compare strings using '==', which checks for reference equality
+      if (name == sameName) {
+         // This will print because both 'name' and 'sameName' refer to the same string literal in the string pool
+         System.out.println("Both 'name' and 'sameName' are the same string literal");
+      }
+
+      if (name == newName) {
+         System.out.println("Both 'name' and 'newName' are the same object");
+      } else {
+         System.out.println("Both 'name' and 'newName' are not the same object");
+      }
+
+      // Compare strings using '.equals()', which checks for value equality
+      if (name.equals(newName)) {
+         System.out.println("Both 'name' and 'newName' have the same value");
+      } else {
+         System.out.println("Both 'name' and 'newName' do not have the same value");
+      }
+   }
+}
