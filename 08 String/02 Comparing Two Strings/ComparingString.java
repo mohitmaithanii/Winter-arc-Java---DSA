14e7ee1d4eb86fde38328fdc420f4973aeac8312
mohitmaithanii <mohitmaithani5@gmail.com>
2024-10-11 16:45:13 +0530

    add two Strings

--- 08 String/02 Comparing Two Strings/ComparingString.java
+++ 08 String/02 Comparing Two Strings/ComparingString.java
@@ -1,17 +1,21 @@
 // Comparing Strings in Java :
 
+import java.util.Scanner;
+
 public class ComparingString {
 
    public static void main(String[] args) {
 
-      // Create two strings with the same value, but one is a literal and the other is created using the 'new' keyword
-      String name = "Mohit"; 
-      String sameName = "Mohit"; 
-      String newName = new String("Mohit"); 
+      // Create two strings with the same value, but one is a literal and the other is
+      // created using the 'new' keyword
+      String name = "Mohit";
+      String sameName = "Mohit";
+      String newName = new String("Mohit");
 
       // Compare strings using '==', which checks for reference equality
       if (name == sameName) {
-         // This will print because both 'name' and 'sameName' refer to the same string literal in the string pool
+         // This will print because both 'name' and 'sameName' refer to the same string
+         // literal in the string pool
          System.out.println("Both 'name' and 'sameName' are the same string literal");
       }
 
@@ -27,5 +31,18 @@ public class ComparingString {
       } else {
          System.out.println("Both 'name' and 'newName' do not have the same value");
       }
+
+      // add two strings:
+      System.out.println("Enter your First Name : ");
+
+      Scanner sc = new Scanner(System.in);
+      String firstName = sc.nextLine();
+
+      System.out.println("Enter your Last Name : ");
+      String lastName = sc.nextLine();
+
+      System.out.println("Your full Name is " + firstName + " " + lastName);
+
+      sc.close();
    }
 }
