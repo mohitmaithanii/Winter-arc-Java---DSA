23d991d4605da8a3c5492071087377a176df7d02
mohitmaithanii <mohitmaithani5@gmail.com>
2024-10-13 22:48:09 +0530

    add some notes

--- 09 OOPS - 1/01 Classes and Objects/MyClass.java
+++ 09 OOPS - 1/01 Classes and Objects/MyClass.java
@@ -1,47 +1,65 @@
-// Classes in Java :
+//  classes in Java :
 
 public class MyClass {
 
+   // This is the main method where the program starts
    public static void main(String[] args) {
 
+      // we use the 'new' keyword to create a new object
       Dog d1 = new Dog();
+      // we use dot notation to access the properties and methods of an object
       d1.name = "Tommy";
+      // Methods are functions that belong to a class or object
       d1.bark();
 
+      // Create a new Dog object called d2
       Dog d2 = new Dog();
+      // Set the name of d2 to "leo"
       d2.name = "leo";
       d2.walk();
 
+      // Create a new Complex object called num1
       Complex num1 = new Complex();
+      // Set the real part of num1 to 3
       num1.a = 3;
+      // Set the imaginary part of num1 to 5
       num1.b = 5;
       num1.print();
    }
 }
 
+// This is a class to represent complex numbers
 class Complex {
-   int a, b;
+   // Real part of the complex number
+   // In Java, we use 'int' to declare an integer variable
+   int a;
+   // Imaginary part of the complex number
+   int b;
 
+   // Method to print the complex number
+   // In Java, we use 'void' to declare a method that does not return a value
    void print() {
       System.out.println("a = " + a + ", b = " + b);
    }
 }
 
+// This is a class to represent dogs
 class Dog {
+   // Name of the dog
+   // In Java, we use 'String' to declare a string variable
    String name;
+   // Age of the dog
    int age;
+   // Color of the dog
    String color;
 
+   // Method to make the dog walk
    void walk() {
       System.out.println(name + " is walking.");
-
    }
 
+   // Method to make the dog bark
    void bark() {
       System.out.println(name + " is barking.");
    }
-}
-
-class Cat {
-
 }
