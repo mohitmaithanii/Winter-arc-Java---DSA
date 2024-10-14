455bc5c49000d61e05d991a3bc279cae19fb9505
mohitmaithanii <mohitmaithani5@gmail.com>
2024-10-14 23:14:35 +0530

    add some rule of method overloading

--- 09 OOPS - 1/02 Method Overloading/MethodOverloading.java
+++ 09 OOPS - 1/02 Method Overloading/MethodOverloading.java
@@ -1,55 +1,58 @@
-// Method Overloading in Java : 
+// Demonstration of Method Overloading in Java
 
 public class MethodOverloading {
 
    public static void main(String[] args) {
 
+      // Create an instance of the Greet class
       Greet obj = new Greet();
+
+      // Call the no-argument method
       obj.greetings();
+
+      // Call the method with one String argument
       obj.greetings("Mohit");
+
+      // Call the method with one String and one int argument
       obj.greetings("Amit", 5);
    }
 }
 
 class Greet {
-   // Method with no arguments
+   // Method with no arguments: default greeting
    void greetings() {
       System.out.println("Hello! Good Morning.");
    }
 
-   // Method with one String argument
+   // Method with one String argument: personalized greeting
    void greetings(String name) {
       System.out.println("Hello " + name + "! Good Morning.");
    }
 
-   // Method with one String and one int argument
+   // Method with one String and one int argument: repeated personalized greeting
    void greetings(String name, int count) {
       for (int i = 0; i < count; i++) {
          System.out.println("Hello " + name + "! Good Morning.");
-
       }
-
    }
 }
 
 /*
+ * Method Overloading Rules:
  * 
- * 1. Two or more methods can have the same name inside the same class if they
- * accept different arguments.
- 
-
- * 2. Methods overloading is achieved by either:
- * (a) changing the number of arguments.
- * (b) or changing the data type of arguments.
- 
-  
- * 3. It is not method overloading if we only change the return type of methods.
- * There must be differences in the number of parameters.
- 
-
- * 4. Method overloading is a compile-time polymorphism, meaning the method to
- * be called is determined at compile time.
- 
- 
+ * 1. Multiple methods can have the same name inside the same class if they
+ * have different parameter lists.
+ * 
+ * 2. Methods can be overloaded by either:
+ * (a) changing the number of parameters.
+ * (b) or changing the data type of parameters.
+ * 
+ * 3. Changing only the return type of methods is not considered method
+ * overloading.
+ * There must be differences in the parameter lists.
+ * 
+ * 4. Method overloading is a form of compile-time polymorphism, meaning the
+ * method to be called is determined at compile time.
+ * 
  * 5. It is also known as compile-time binding or static binding.
  */
