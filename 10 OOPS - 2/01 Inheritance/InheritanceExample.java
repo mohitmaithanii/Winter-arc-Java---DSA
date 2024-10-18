2b8550dddd02febc801c34d0bf8073680570d83e
mohitmaithanii <mohitmaithani5@gmail.com>
2024-10-18 15:44:31 +0530

    add some comments

--- 10 OOPS - 2/01 Inheritance/InheritanceExample.java
+++ 10 OOPS - 2/01 Inheritance/InheritanceExample.java
@@ -1,26 +1,34 @@
 // Base class (Parent class)
 class Animal {
+   // Method to describe eating behavior
    void eat() {
-      System.out.println("This animal eats food.");
+      System.out.println("This animal eats food."); // Default behavior for animals
    }
 }
 
-// Derived class (Child class)
+// Derived class (Child class) that extends Animal
 class Dog extends Animal {
+   // Overriding the eat method to provide specific behavior for Dog
    @Override
    void eat() {
-      System.out.println("The dog eats dog food.");
+      System.out.println("The dog eats dog food."); // Specific behavior for dogs
    }
 
+   // Method specific to Dog class
    void bark() {
-      System.out.println("The dog barks.");
+      System.out.println("The dog barks."); // Behavior unique to dogs
    }
 }
 
 public class InheritanceExample {
    public static void main(String[] args) {
+      // Creating an instance of Dog class
       Dog dog = new Dog();
-      dog.eat(); // Calls the overridden method
+
+      // Calling the overridden eat method from Dog class
+      dog.eat(); // Calls the overridden method in Dog class
+
+      // Calling the bark method from Dog class
       dog.bark(); // Calls the method from Dog class
    }
 }
