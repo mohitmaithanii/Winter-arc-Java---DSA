242d9c61193fab2da7c94601b3a4c1ffb1c1a88a
mohitmaithanii <mohitmaithani5@gmail.com>
2024-10-21 23:03:14 +0530

    "Added comments to explain instance variables and method calls in Animal and Dog classes."

--- 10 OOPS - 2/02 Super Keyword/SuperKeyword.java
+++ 10 OOPS - 2/02 Super Keyword/SuperKeyword.java
@@ -2,7 +2,7 @@
 
 // Parent class representing a generic Animal
 class Animal {
-   String name;
+   String name; // Instance variable to hold the name of the animal
 
    // Constructor of the parent class to initialize the animal's name
    Animal(String name) {
@@ -17,26 +17,26 @@ class Animal {
 
 // Child class representing a Dog, which is a specific type of Animal
 class Dog extends Animal {
-   String breed;
+   String breed; // Instance variable to hold the breed of the dog
 
    // Constructor of the child class to initialize both name and breed
    Dog(String name, String breed) {
-      super(name);
-      this.breed = breed;
+      super(name); // Call the constructor of the parent class (Animal) to set the name
+      this.breed = breed; // Assign the provided breed to the instance variable
    }
 
    // Method to display the dog's information, including its breed
    void display() {
-      super.display();
-      System.out.println("Dog Breed: " + breed);
+      super.display(); // Call the display method of the parent class to show the animal's name
+      System.out.println("Dog Breed: " + breed); 
    }
 }
 
 // Main class to test the functionality of the Dog class
 public class SuperKeyword {
    public static void main(String[] args) {
-      Dog dog = new Dog("Buddy", "Golden Retriever");
-      dog.display();
+      Dog dog = new Dog("Buddy", "Golden Retriever"); // Create a new Dog object
+      dog.display(); // Call the display method to show the dog's information
    }
 }
 
